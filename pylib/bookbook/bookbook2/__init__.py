214902f7141fb5ae93cdf47e3373f15f3bab2fcf
ShixiangWang <wangshx@shanghaitech.edu.cn>
2019-02-23 23:29:40 +0800

    update & upload to pypi

--- pylib/bookbook/bookbook2/__init__.py
+++ pylib/bookbook/bookbook2/__init__.py
@@ -1,4 +1,4 @@
 """Tool for converting ipython notebooks to html or pdf chapters
 """
 
-__version__ = '0.1.0'
+name = "bookbook2"
--- pylib/bookbook/setup.py
+++ pylib/bookbook/setup.py
@@ -9,17 +9,27 @@ requires = [
     'pandocfilters'
 ]
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name='bookbook2',
     version='0.1.0',
-    description='Convert ipython notebooks to html/pdf chapters',
     author='Shixiang Wang',
     author_email='w_shixiang@163.com',
+    description='Convert ipython notebooks to html or pdf chapters',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     url='https://github.com/ShixiangWang/bookbook2',
-    keywords='book converter render',
     packages=["bookbook2"],
     package_dir={'bookbook2': 'bookbook2'},
     package_data={'bookbook2': ['html_index.tpl']},
     include_package_data=True,
-    install_requires=requires
+    install_requires=requires,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Topic :: Text Processing :: Markup"
+    ]
 )
